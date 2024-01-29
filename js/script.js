71803c6c7499888351c113a7a24e1812ad34009b
NeMurloc <104652243+NeMurloc@users.noreply.github.com>
2024-01-29 17:02:59 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,4 +4,16 @@ tg.expand();
 
 tg.MainButton.textColor = "#FFFFFF";
 tg.MainButton.color = "#50A0FF";
+tg.MainButton.text = "Выбрать точку";
+
+var radioButtons = document.querySelectorAll('input[type="radio"][name="address"]');
+
+radioButtons.forEach(function (radioButton) {
+  radioButton.addEventListener("change", function () {
+    if (this.checked) {
+        tg.MainButton.show();
+    }
+  });
+});
+
 tg.MainButton.show();
